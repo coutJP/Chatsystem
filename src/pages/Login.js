75fbd0c0e89ac2f6832f5729ff9894e4f0be54d5
coutJP <jeanpaul_mansour@outlook.com>
2023-02-14 22:02:23 +0200

    password and email alert are done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,6 +7,7 @@ import Add from "../img/addAvatar.png"
 const Login = () => {
 
   const [err,setErr]=useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate=useNavigate();
 
   const handleSubmit=async (e)=>{
@@ -18,11 +19,25 @@ const Login = () => {
       await signInWithEmailAndPassword(auth, email, password);
       navigate('/')
 
-    }catch(err){
-  setErr(true);
-  console.log("thiserr")
-  }
+    }catch (err) {
+      console.log(err.message)
+      if (err.message === "Firebase: Error (auth/wrong-password).") {
+        setErr(true);
+        setErrorMessage("Incorrect password.");
+      } else if(err.message === "Firebase: Error (auth/user-not-found)."){
+        setErr(true);
+        setErrorMessage("This user doesn't exist:( \t Please sign up:)"); 
+      }else{
+        setErr(true);
+        setErrorMessage(err.message);
+      }
+    }
   };
+  // setErr(true);
+  
+  // console.log("thiserr")
+  
+  
  
 
   return (
@@ -36,13 +51,14 @@ const Login = () => {
             <input type="password" placeholder='password'/>
             <input style={{display:"none"}} type="file" id='file' />
             <button className='firstbut'>Sign in</button>
-            {err && <span>Something went error</span>}
-
+              {err && (
+          <p>{errorMessage}</p>
+        )}
         </form>
         <p>You don't have an account? <Link to='/register'>Sign up</Link></p>
       </div>
     </div>
   )
-}
 
+};
 export default Login
