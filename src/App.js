99982c562ee94f301883534ea68dca812274564a
coutJP <jeanpaul_mansour@outlook.com>
2023-02-15 19:10:20 +0200

    push notification from firebae

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { messaging } from './firebase';
+import { getToken } from 'firebase/messaging';
 import logo from './logo.svg';
 import './App.css';
 import "./Style.css"
@@ -9,6 +12,25 @@ import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
 function App() {
+  ////notification////////
+  async function requestPermission(){
+    const permission=await Notification.requestPermission();
+    if(permission==='granted'){
+      //generate token
+      const token=await getToken(messaging,{vapidKey:'BA8Zhozbdu802fEreIjz40ibZZXpb9k-WRMn7K4QITvZ116e2HvHQKfT0qWybKmTTn0PNSb1PLPe0qP2L2JG0Gc'})
+      console.log("Token gen",token);
+/////////////send this token to server db
+    }else if(permission==="denied"){
+      alert("you denied for notification")
+    }
+  }
+  useEffect(()=>{ 
+    //req the user for notification permission
+    requestPermission();
+  },[])
+
+
+  ////////////
   const {currentUser}=useContext(AuthContext)
   const ProtectedRoute=({children})=>{
     if (!currentUser){
